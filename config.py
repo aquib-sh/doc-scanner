b65806259c6f7990d87650a3dea7d7525432bcc2
Aquib <shaikhaquib394@gmail.com>
2021-11-25 12:21:38 +0530

    Add configuration file

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+data_dir = os.path.join(os.getcwd(), "internal")
+# ======================= CACHE FILE  =============================
+cache_file = "cache.json"
+cache_path = os.path.join(data_dir, cache_file)
+# ======================= GOOGLE APIs =============================
+google_token_file  = 'token.json'
+client_secret_file = 'client_secret.json'
+
+client_secret_path    = os.path.join(data_dir, client_secret_file)
+google_api_token_path = os.path.join(data_dir, google_token_file)
+
+google_api_scope = ['https://www.googleapis.com/auth/gmail',
+                    'https://www.googleapis.com/auth/documents']
