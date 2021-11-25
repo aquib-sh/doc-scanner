8caef39e3803a359437adbf0e80fb80a74a6a445
Aquib <shaikhaquib394@gmail.com>
2021-11-25 12:42:15 +0530

    Add token generator

---
+++ generate.py
@@ -0,0 +1,7 @@
+import config
+from services.google.token import TokenGenerator
+
+generator = TokenGenerator()
+
+token = generator.generate_google_api_token(config.client_secret_path, config.google_api_scope)
+generator.export_token(token, "internal/token.json")
