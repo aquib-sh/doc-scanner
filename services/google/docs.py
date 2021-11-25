536e8fba00e6863e1f5fb06e3c6ff920eab208fb
Aquib <shaikhaquib394@gmail.com>
2021-11-25 21:22:25 +0530

    Add methods to fetch document text from link

--- services/google/docs.py
+++ services/google/docs.py
@@ -1,3 +1,4 @@
+import re
 from services.google.service_builder import GoogleServices
 
 class GoogleDocs(GoogleServices):
@@ -16,5 +17,46 @@ class GoogleDocs(GoogleServices):
 
     def start_service(self, version='v1'):
         """Builds Google Docs Service."""
-        super().start_service("docs", version)
+        return super().start_service("docs", version)
 
+    def __get_document_body(self, document_id:str):
+        """Returns document body."""
+        doc = self.service.documents().get(
+            documentId=document_id).execute()
+        return doc.get('body')
+
+    def get_text_from_document(self, document_id:str) -> str:
+        """Extracts text from the document
+
+        Parameters
+        ----------
+        document_id: str
+            id of Google Docs document.  
+        """
+        document = ""
+        doc_content = self.__get_document_body(document_id)
+        for content in doc_content['content']:
+            if 'paragraph' in content:
+                para = content['paragraph']
+                elems = para['elements']
+                for elem in elems:
+                    document += elem['textRun']['content']
+        return document
+
+    def __get_id_from_link(self, link:str) -> str:
+        """Extracts ID from link."""
+        pattern = r"[https:\/\/]?docs\.google\.com\/document\/d\/(.*)\/edit"
+        match = re.search(pattern, link)
+        return match.group(1)
+
+    def get_text_from_document_using_link(self, link:str) -> str:
+        """Extracts text from the document using link as parameter
+
+        Parameters
+        ----------
+        link: str
+            link of Google Docs document.  
+        """
+        doc_id = self.__get_id_from_link(link)
+        content =  self.get_text_from_document(doc_id)
+        return content
