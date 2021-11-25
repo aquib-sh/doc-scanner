d17a5cccb0c0a63299e29c45879100e414ac2b60
Aquib <shaikhaquib394@gmail.com>
2021-11-25 12:44:16 +0530

    add .readonly to gmail scope

--- config.py
+++ config.py
@@ -11,5 +11,5 @@ client_secret_file = 'client_secret.json'
 client_secret_path    = os.path.join(data_dir, client_secret_file)
 google_api_token_path = os.path.join(data_dir, google_token_file)
 
-google_api_scope = ['https://www.googleapis.com/auth/gmail',
+google_api_scope = ['https://www.googleapis.com/auth/gmail.readonly',
                     'https://www.googleapis.com/auth/documents']
