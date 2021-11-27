b02579ac4c5ba6d764f6d552de632a5a9da7403d
Aquib <shaikhaquib394@gmail.com>
2021-11-27 15:10:47 +0530

    Add method to create a text document

--- kernel.py
+++ kernel.py
@@ -16,3 +16,7 @@ class DocsKernel:
    def fetch_document_text(self, link:str) -> str:
       raw_text = self.docs.get_text_from_document_using_link(link)
       return raw_text
+
+   def create_document(self, title:str, text:str):
+      documentID = self.docs.create_empty_document(title)
+      self.docs.insert_into_document(documentID, text)
