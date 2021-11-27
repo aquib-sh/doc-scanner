64ceb73d51e14c42fd6bdb6d15c0b21872acf920
Aquib <shaikhaquib394@gmail.com>
2021-11-27 13:12:54 +0530

    Add input file path to config

--- config.py
+++ config.py
@@ -4,6 +4,8 @@ data_dir = os.path.join(os.getcwd(), "internal")
 # ======================= CACHE FILE  =============================
 cache_file = "cache.json"
 cache_path = os.path.join(data_dir, cache_file)
+# ======================= INPUT FILE  =============================
+input_file = "input.json"
 # ======================= GOOGLE APIs =============================
 google_token_file  = 'token.json'
 client_secret_file = 'client_secret.json'
