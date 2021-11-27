20090f33748e58615a720183872f259fc2634235
Aquib <shaikhaquib394@gmail.com>
2021-11-27 13:13:27 +0530

    Add case sensitivity option for search_sentences

--- parser.py
+++ parser.py
@@ -13,9 +13,17 @@ class DocParser:
                 prev = ""
                 yield temp 
 
-    def search_sentences(self, search_key:str) -> list:
+    def search_sentences(self, search_key:str, case_sensitive=True) -> list:
         results = []
         for sentence in self.get_sentences():
-            if search_key in sentence:
+            present = False
+            
+            if not case_sensitive:
+                present = (search_key.lower() in sentence.lower())
+            else:
+                present = (search_key in sentence)
+
+            if (present):
                 results.append(sentence)
+
         return results 
